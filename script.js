6cdc9f9f3180fcd843f7a3c904ece4dd40ee2c92
PedroZanette <pedro_zanette@estudante.sesisenai.org.br>
2024-05-29 09:56:22 -0300

    Versão 1

---
+++ script.js
@@ -0,0 +1,11 @@
+function Irlogin(){
+    window.location.href="../index.html"
+}
+
+function Ircadastro(){
+    window.location.href="pages/cadastro.html"
+}
+
+function login(){
+    window.location.href="pages/"
+}
